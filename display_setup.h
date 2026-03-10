795fc0f7715ae3baee34edea2208ebca81231e98
mvasa134 <mvasa134@gmail.com>
2026-03-10 11:17:47 +0200

    Update display_setup.h

--- display_setup.h
+++ display_setup.h
@@ -1,3 +1,11 @@
 #pragma once
 
-void display_init();
+// display_setup.h
+// В режиме ESPHome-хоста этот файл содержит только
+// инициализацию железа (PSRAM, подсветка, lcd.begin).
+// lv_init(), lv_display_create() и flush-callback
+// полностью берёт на себя ESPHome mipi_dsi компонент.
+
+void display_hw_init();   // переименовано: НЕ трогает LVGL
+void go_to_sleep();
+void wake_up();
